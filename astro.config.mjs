f92669995679d7d951b3d755d911ce83eeb351f1
Hrushal Nikhare <hrushalnikhare@gmail.com>
2023-05-05 19:02:31 +0530

    well well well

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://beta.hrushalnikhare.xyz',
-	integrations: [mdx(), sitemap()],
+  output: "server",
+  adapter: vercel(),
+  site: "https://beta.hrushalnikhare.xyz",
+  integrations: [mdx(), sitemap()],
 });
