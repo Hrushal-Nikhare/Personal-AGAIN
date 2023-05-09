7576d2f433331dcf4decb23e793b19a556459a5f
Hrushal Nikhare <Hrushalnikhare@gmail.com>
2023-05-09 11:11:43 +0530

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel(),
+  // output: "server",
+  // adapter: vercel(),
   site: "https://beta.hrushalnikhare.xyz",
   integrations: [mdx(), sitemap()],
 });
